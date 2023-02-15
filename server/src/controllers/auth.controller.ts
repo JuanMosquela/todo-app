ed5e2d6a57e892c3f6dabe49cebec39e296cf813
Juan Mosquella <jmosquella11@gmail.com>
2023-02-15 14:06:58 -0300

    ruta loagin y register terminada

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import pool from "../config/pg.config";
+import generateToken from "../helpers/generate-token";
 
 const register = async (req: Request, res: Response) => {
   try {
@@ -9,15 +10,18 @@ const register = async (req: Request, res: Response) => {
     const salt = bcrypt.genSaltSync(10);
     const hashed_password = bcrypt.hashSync(password, salt);
 
-    const user = await pool.query(
-      "INSERT INTO users (first_name, last_name, email, hashed_password) VALUES($1, $2, $3, $4)",
-      [first_name, last_name, email, hashed_password]
-    );
-
-    res.status(200).json({
-      msg: "User registered succesfully",
-      user,
-    });
+    try {
+      const user = await pool.query(
+        "INSERT INTO users (first_name, last_name, email, hashed_password) VALUES($1, $2, $3, $4)",
+        [first_name, last_name, email, hashed_password]
+      );
+      res.status(200).json({
+        msg: "User registered succesfully",
+        user,
+      });
+    } catch (error) {
+      return res.status(400).send({ msg: "algo malo paso" });
+    }
   } catch (error) {
     res.status(500).send(error);
   }
@@ -25,7 +29,36 @@ const register = async (req: Request, res: Response) => {
 
 const login = async (req: Request, res: Response) => {
   try {
-    res.status(200).json({ msg: " todo bien" });
+    const { email, password } = req.body;
+
+    const user: any = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (user.rowCount === 0) {
+      return res.status(404).send({ error: "User not found" });
+    }
+
+    // Compare password
+
+    const check_password = await bcrypt.compare(
+      password,
+      user.rows[0].hashed_password
+    );
+
+    // console.log(check_password);
+
+    if (!check_password) {
+      return res.status(401).json({ msg: "Invalid email or password" });
+    }
+
+    const token = generateToken(user.id);
+
+    res.status(200).json({
+      msg: "user login succesfully",
+      result: user.rows,
+      token,
+    });
   } catch (error) {
     res.status(500).send(error);
   }
