a97afc011ca657db1979af99c779e625cc359dad
Juan Mosquella <jmosquella11@gmail.com>
2023-03-08 12:06:03 -0300

    actualizado url

--- client/src/redux/api/emptyApi.ts
+++ client/src/redux/api/emptyApi.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const emptyApi = createApi({
   reducerPath: "todoApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://todo-app-iz14.onrender.com/api" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://todo-app-iz14.onrender.com/api",
+  }),
 
   endpoints: () => ({}),
 });
