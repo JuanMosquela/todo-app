0d6c2c955399b7a8a16ae9339c008eec4234df32
Juan Mosquella <jmosquella11@gmail.com>
2023-03-08 13:33:58 -0300

    cambios en codfig

--- server/src/config/pg.config.ts
+++ server/src/config/pg.config.ts
@@ -5,7 +5,7 @@ import "dotenv/config";
 const pool = new Pool({
   user: "postgres",
   password: process.env.PASSWORD,
-  host: "******-a.oregon-postgres.render.com",
+  host: "localhost",
   port: 5432,
   database: "todo-app",
 });
