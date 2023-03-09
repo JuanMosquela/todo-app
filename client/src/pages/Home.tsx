53445e8d4b34546dfe920a441cfcb415e3b8a6de
Juan Mosquella <jmosquella11@gmail.com>
2023-03-09 14:20:53 -0300

    cambios en ui responsive

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,10 +7,12 @@ import TodoModal from "../components/TodoModal";
 const Home = () => {
   return (
     <div className=" bg-slate-200  flex justify-center min-h-screen py-10  ">
-      <div className=" px-10 py-10 md:px-4   bg-white rounded-xl  w-[200px] md:w-[700px]    ">
+      <div className=" md:px-10 px-4 py-10   bg-white rounded-xl  w-[200px] md:w-[700px]    ">
         <nav className="flex justify-between items-center mb-6 ">
-          <h1 className=" font-bold uppercase text-3xl text-blue">Todo App</h1>
-          <div className="flex gap-4">
+          <h1 className=" font-bold uppercase md:text-3xl text-2xl text-blue">
+            Todo App
+          </h1>
+          <div className="flex md:gap-4 gap-2">
             <PopUp />
             <button className="flex justify-center items-center text-xl w-10 h-10">
               <IoIosOptions />
